ee79dec3548c0e7d367f9fd9a96c68d759c11ad5
DavidsonIzefler <davidson.izefler@gmail.com>
2020-07-04 12:18:33 +0000

    Adding localization + LandMark

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.nav.controller.NotFound", {
-      onInit: function () {
-      }
-   });
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent"
+], function (Controller, UIComponent) {
+	"use strict";
+
+	return Controller.extend("openSAP.movies.controller.NotFound", {
+
+		onNavBack : function () {
+			UIComponent.getRouterFor(this).navTo("Home");
+		}
+
+	});
+
 });
